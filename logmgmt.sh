71491f29e65e40d879da9b9ea7c044f87b991cfb
Bhavin Gandhi <bhavin@infracloud.io>
2022-07-08 19:11:35 +0530

    Implement the rotate subcommand
    Signed-off-by: Bhavin Gandhi <bhavin@infracloud.io>

--- logmgmt.sh
+++ logmgmt.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o errexit -o pipefail
+
 function info() {
   echo "[INFO] $@"
 }
@@ -13,6 +15,17 @@ function gen_logs() {
   for (( n = 0; n < $2; n++ )); do
     echo "The quick brown fox jumps over the lazy dog." >> "./logs/$1"
   done
+}
+
+function rotate_logs() {
+  file="./logs/$1"
+  threshold="$2"
+  new_file="${file}-$(date +%s)"
+  lines_in_log="$(cat "${file}" | wc -l)"
+  if [[ ${lines_in_log} -gt ${threshold} ]]; then
+    mv "${file}" "${new_file}"
+  fi
+  info "The '${file}' has been renamed to '${new_file}'."
 }
 
 if [[ $(id -u) -eq 0 ]]; then
@@ -29,4 +42,8 @@ fi
 
 if [[ "$1" == "gen" ]]; then
   gen_logs "earth-log" 50
+fi
+
+if [[ "$1" == "rotate" ]]; then
+  rotate_logs "earth-log" 20
 fi
